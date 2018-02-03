20265ba4a63b6d239809ad597a949a653192e822
LightDance <773617349@qq.com>
2018-02-03 19:09:39 +0800

    test

--- app/src/main/java/com/example/lightdance/takemyadvice/page/base/BasePresenter.java
+++ app/src/main/java/com/example/lightdance/takemyadvice/page/base/BasePresenter.java
@@ -1,8 +1,18 @@
 package com.example.lightdance.takemyadvice.page.base;
 
 /**
- * Created by LightDance on 2018/2/2.
+ * @author  LightDance
+ * @date 2018/2/2.
+ * @description presenter的基类
  */
 
 public interface BasePresenter {
+
+    /**
+     * 和 View (Fragment) 的生命周期结合
+     * TODO 内存溢出问题未解决，描述同{@see BaseView}
+     */
+    void start();
+
+    void destroy();
 }
--- app/src/main/java/com/example/lightdance/takemyadvice/page/base/BaseView.java
+++ app/src/main/java/com/example/lightdance/takemyadvice/page/base/BaseView.java
@@ -11,10 +11,15 @@ public interface BaseView<T extends BasePresenter> {
     /**
      * 将指定的presenter与view绑定，由于每个 View 都对应一个 Presenter，
      * 所以把 bindPresenter 写在 BaseView 中减少重复代码
-     * TODO 注：前两天看到绑定后为防止页面被finish掉而未解除绑定的情况出现，要搞一个“弱引用”出来，可惜我不会:(
+     * TODO 注：绑定后为防止页面被finish掉而未解除绑定的情况出现，要搞一个“弱引用”出来，可惜我不会:(
      * @param presenter
      */
-     void bindPresenter(T presenter);
+    void bindPresenter(T presenter);
 
-
+    /**
+     * 参数表详见 {@see com.example.lightdance.takemyadvice.app.MessageLevel}
+     * @param msg
+     * @param level 默认为TOAST
+     */
+    void showMessage(String msg , int level);
 }
